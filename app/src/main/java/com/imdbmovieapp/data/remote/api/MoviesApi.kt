721ebcec17e7f30afbbc0e76c459d3b99db8cc74
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-17 13:06:33 +0400

    add Responses

--- app/src/main/java/com/imdbmovieapp/data/remote/api/MoviesApi.kt
+++ app/src/main/java/com/imdbmovieapp/data/remote/api/MoviesApi.kt
@@ -1,28 +1,29 @@
 package com.imdbmovieapp.data.remote.api
 
-import com.imdbmovieapp.data.remote.dto.detailMovieDto.DetailMovieDto
-import com.imdbmovieapp.data.remote.dto.genreMovieDto.GenreMovieDto
+import com.imdbmovieapp.data.remote.dto.detailMovieDto.MovieDetailDto
+import com.imdbmovieapp.data.remote.dto.detailMovieDto.MovieGenreDto
 import com.imdbmovieapp.data.remote.dto.popularMoviesDto.PopularMoviesDto
 import com.imdbmovieapp.data.remote.dto.searchMoviesDto.SearchMoviesDto
 import com.imdbmovieapp.data.remote.dto.topRatedMoviesDto.TopRatedMoviesDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.Response
 
 interface MoviesApi {
 
     @GET("/3/movie/popular")
-    suspend fun getPopularMovies(): List<PopularMoviesDto>
+    suspend fun getPopularMovies(): Response<List<PopularMoviesDto>>
 
     @GET("/3/movie/top_rated")
-    suspend fun getTopRatedMovies(): List<TopRatedMoviesDto>
+    suspend fun getTopRatedMovies(): Response<List<TopRatedMoviesDto>>
 
     @GET("/3/movie/{movie_id}")
-    suspend fun getDetailMovie(@Path("movie_id") movieId: Int): DetailMovieDto
+    suspend fun getDetailMovie(@Path("movie_id") movieId: Int): Response<MovieDetailDto>
 
     @GET("/3/search/movie")
-    suspend fun getSearchMovies(@Query("query") query: String): List<SearchMoviesDto>
+    suspend fun getSearchMovies(@Query("query") query: String): Response<List<SearchMoviesDto>>
 
     @GET("/3/genre/movie/list")
-    suspend fun getGenres(): List<GenreMovieDto>
+    suspend fun getGenres(): Response<List<MovieGenreDto>>
 }
