2a83be24084463019eadda6d74a3c78b1ef51b7d
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-19 15:20:27 +0400

    inject dao directly

--- app/src/main/java/com/imdbmovieapp/data/repository/FavoriteMovieRepositoryImpl.kt
+++ app/src/main/java/com/imdbmovieapp/data/repository/FavoriteMovieRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.imdbmovieapp.data.repository
 
-import com.imdbmovieapp.data.local.database.FavoriteMovieDatabase
+import com.imdbmovieapp.data.local.dao.FavoriteMovieDao
 import com.imdbmovieapp.data.local.mapper.MovieDomainToMovieEntityMapper
 import com.imdbmovieapp.data.local.mapper.MovieEntityToMovieDomainMapper
 import com.imdbmovieapp.domain.model.FavoriteMovieDomain
@@ -9,24 +9,24 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class FavoriteMovieRepositoryImpl(
-    private val favoriteMovieDatabase: FavoriteMovieDatabase,
+    private val favoriteMovieDao: FavoriteMovieDao,
     private val movieEntityToMovieDomainMapper: MovieEntityToMovieDomainMapper,
     private val movieDomainToMovieEntityMapper: MovieDomainToMovieEntityMapper
 ) : FavoriteMovieRepository {
     override suspend fun insert(favoriteMovieDomain: FavoriteMovieDomain) {
-        favoriteMovieDatabase.favoriteMovieDao().insertMovie(
+        favoriteMovieDao.insertMovie(
             movieDomainToMovieEntityMapper.mapModel(favoriteMovieDomain)
         )
     }
 
     override suspend fun delete(favoriteMovieDomain: FavoriteMovieDomain) {
-        favoriteMovieDatabase.favoriteMovieDao().deleteMovie(
+        favoriteMovieDao.deleteMovie(
             movieDomainToMovieEntityMapper.mapModel(favoriteMovieDomain)
         )
     }
 
     override fun getAllMovies(): Flow<List<FavoriteMovieDomain>> {
-        return favoriteMovieDatabase.favoriteMovieDao().getAllMovies().map {
+        return favoriteMovieDao.getAllMovies().map {
             movieEntityToMovieDomainMapper.mapToList(it)
         }
     }
