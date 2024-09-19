1f2a1b9bd52d52577c3b6359ebfcd3c895e2ea6f
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-19 15:20:13 +0400

    generics all Caps

--- app/src/main/java/com/imdbmovieapp/domain/base/BaseUseCase.kt
+++ app/src/main/java/com/imdbmovieapp/domain/base/BaseUseCase.kt
@@ -1,5 +1,5 @@
 package com.imdbmovieapp.domain.base
 
-interface BaseUseCase<in Data, out T> {
-    abstract suspend operator fun invoke(data: Data? = null): T
+interface BaseUseCase<in DATA, out T> {
+    abstract suspend operator fun invoke(data: DATA? = null): T
 }
