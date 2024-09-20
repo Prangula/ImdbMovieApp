8779b27b0c27db41b6e1373222e08da5d918d2a6
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-20 13:37:50 +0400

    remove with(model) because i have only one parameter.

--- app/src/main/java/com/imdbmovieapp/data/remote/mapper/domainToDto/GenreDomainToGenreDtoMapper.kt
+++ app/src/main/java/com/imdbmovieapp/data/remote/mapper/domainToDto/GenreDomainToGenreDtoMapper.kt
@@ -1,15 +1,13 @@
 package com.imdbmovieapp.data.remote.mapper.domainToDto
 
-import com.imdbmovieapp.data.remote.dto.genreMovieDto.GenreMovieDto
+import com.imdbmovieapp.data.remote.dto.movieGenreDto.MovieGenreDto
 import com.imdbmovieapp.domain.model.GenreMoviesDomain
 import com.imdbmovieapp.utils.BaseMapper
 
-class GenreDomainToGenreDtoMapper : BaseMapper<GenreMoviesDomain, GenreMovieDto> {
-    override fun mapModel(model: GenreMoviesDomain): GenreMovieDto {
-        return with(model) {
-            GenreMovieDto(
-                genres = genres
-            )
-        }
+class GenreDomainToGenreDtoMapper : BaseMapper<GenreMoviesDomain, MovieGenreDto> {
+    override fun mapModel(model: GenreMoviesDomain): MovieGenreDto {
+        return MovieGenreDto(
+            genres = model.genres
+        )
     }
 }
--- app/src/main/java/com/imdbmovieapp/data/remote/mapper/dtoToDomain/GenreDtoToGenreDomainMapper.kt
+++ app/src/main/java/com/imdbmovieapp/data/remote/mapper/dtoToDomain/GenreDtoToGenreDomainMapper.kt
@@ -1,15 +1,13 @@
 package com.imdbmovieapp.data.remote.mapper.dtoToDomain
 
-import com.imdbmovieapp.data.remote.dto.genreMovieDto.GenreMovieDto
+import com.imdbmovieapp.data.remote.dto.movieGenreDto.MovieGenreDto
 import com.imdbmovieapp.domain.model.GenreMoviesDomain
 import com.imdbmovieapp.utils.BaseMapper
 
-class GenreDtoToGenreDomainMapper : BaseMapper<GenreMovieDto, GenreMoviesDomain> {
-    override fun mapModel(model: GenreMovieDto): GenreMoviesDomain {
-        return with(model) {
-            GenreMoviesDomain(
-                genres = genres
-            )
-        }
+class GenreDtoToGenreDomainMapper : BaseMapper<MovieGenreDto, GenreMoviesDomain> {
+    override fun mapModel(model: MovieGenreDto): GenreMoviesDomain {
+        return GenreMoviesDomain(
+            genres = model.genres
+        )
     }
 }
