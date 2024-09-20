d7a104d75b059c8d3f26539d5b0f756daa9b2ece
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-20 13:17:46 +0400

    implement Fragments and nav_graph

---
+++ app/src/main/java/com/imdbmovieapp/presentation/screen/popularMoviesFragment/PopularMoviesFragment.kt
@@ -0,0 +1,7 @@
+package com.imdbmovieapp.presentation.screen.popularMoviesFragment
+
+import androidx.fragment.app.Fragment
+import com.imdbmovieapp.R
+
+class PopularMoviesFragment : Fragment(R.layout.fragment_popular_movies) {
+}
---
+++ app/src/main/java/com/imdbmovieapp/presentation/screen/splashScreenMoviesFragment/SplashScreenMoviesFragment.kt
@@ -0,0 +1,9 @@
+package com.imdbmovieapp.presentation.screen.splashScreenMoviesFragment
+
+import android.annotation.SuppressLint
+import androidx.fragment.app.Fragment
+import com.imdbmovieapp.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenMoviesFragment : Fragment(R.layout.fragment_splash_screen_movies) {
+}
