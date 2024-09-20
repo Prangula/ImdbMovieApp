4ac2d983c52ab4fb8d960369977d58bfbd7628d8
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-20 16:37:16 +0400

    apiBaseUseCase -> BaseUseCase

--- app/src/main/java/com/imdbmovieapp/domain/base/BaseUseCase.kt
+++ app/src/main/java/com/imdbmovieapp/domain/base/BaseUseCase.kt
@@ -3,6 +3,6 @@ package com.imdbmovieapp.domain.base
 import com.imdbmovieapp.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
-interface ApiBaseUseCase<in DATA, T> {
+interface BaseUseCase<in DATA, T> {
     suspend operator fun invoke(data: DATA): Flow<Resource<T>>
 }
