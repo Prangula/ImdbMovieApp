0e5368102140a2cbee3c0ce02751446d876deec6
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 13:12:04 +0400

    return resource<list ...

--- app/src/main/java/com/imdbmovieapp/domain/repository/ApiMovieRepository.kt
+++ app/src/main/java/com/imdbmovieapp/domain/repository/ApiMovieRepository.kt
@@ -5,11 +5,12 @@ import com.imdbmovieapp.domain.model.GenreMoviesDomain
 import com.imdbmovieapp.domain.model.PopularMoviesDomain
 import com.imdbmovieapp.domain.model.SearchMoviesDomain
 import com.imdbmovieapp.domain.model.TopRatedMoviesDomain
+import com.imdbmovieapp.utils.Resource
 
 interface ApiMovieRepository {
-    suspend fun getPopularMovies(): List<PopularMoviesDomain>
-    suspend fun getTopRatedMovies(): List<TopRatedMoviesDomain>
-    suspend fun getDetailMovie(movieId: String): DetailMovieDomain
-    suspend fun getSearchMovies(query: String): List<SearchMoviesDomain>
-    suspend fun getGenres(): List<GenreMoviesDomain>
+    suspend fun getPopularMovies(): Resource<List<PopularMoviesDomain>>
+    suspend fun getTopRatedMovies(): Resource<List<TopRatedMoviesDomain>>
+    suspend fun getDetailMovie(movieId: String): Resource<DetailMovieDomain>
+    suspend fun getSearchMovies(query: String): Resource<List<SearchMoviesDomain>>
+    suspend fun getGenres(): Resource<List<GenreMoviesDomain>>
 }
