b2008a9406a6a6f68ac8af371269b3b4c140b09b
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 14:56:00 +0400

    Add Custom Search Bar

--- app/src/main/java/com/imdbmovieapp/presentation/screen/homeMoviesFragment/HomeMoviesFragment.kt
+++ app/src/main/java/com/imdbmovieapp/presentation/screen/homeMoviesFragment/HomeMoviesFragment.kt
@@ -10,21 +10,4 @@ import com.imdbmovieapp.databinding.FragmentHomeMoviesBinding
 import com.imdbmovieapp.utils.CustomSearchBar
 
 class HomeMoviesFragment : Fragment(R.layout.fragment_home_movies) {
-    private lateinit var binding: FragmentHomeMoviesBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentHomeMoviesBinding.inflate(layoutInflater)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.customSearchBar.showGenreTags()
-
-    }
 }
