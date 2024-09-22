2fde9fa1c122848dcf8b72d6a7b9115d67916c2f
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 13:12:25 +0400

    don't need flow with suspend function

--- app/src/main/java/com/imdbmovieapp/domain/base/BaseUseCase.kt
+++ app/src/main/java/com/imdbmovieapp/domain/base/BaseUseCase.kt
@@ -1,8 +1,7 @@
 package com.imdbmovieapp.domain.base
 
 import com.imdbmovieapp.utils.Resource
-import kotlinx.coroutines.flow.Flow
 
 interface BaseUseCase<in DATA, T> {
-    suspend operator fun invoke(data: DATA): Flow<Resource<T>>
+    suspend operator fun invoke(data: DATA): Resource<T>
 }
