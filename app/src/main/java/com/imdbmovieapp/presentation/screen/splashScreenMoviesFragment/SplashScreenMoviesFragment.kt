bb7dacfd8bb8a39b1abbffae0a5d564e40cdb262
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 19:00:46 +0400

    Create SplashScreen from google recommended way.

--- app/src/main/java/com/imdbmovieapp/presentation/screen/splashScreenMoviesFragment/SplashScreenMoviesFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.imdbmovieapp.presentation.screen.splashScreenMoviesFragment
-
-import android.annotation.SuppressLint
-import androidx.fragment.app.Fragment
-import com.imdbmovieapp.R
-
-@SuppressLint("CustomSplashScreen")
-class SplashScreenMoviesFragment : Fragment(R.layout.fragment_splash_screen_movies) {
-}
