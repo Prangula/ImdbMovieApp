7d4a8955d4228199a5a5aeb76d3a3c984644feee
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 18:02:44 +0400

    create ChipGroup for Detail Fragment

--- app/src/main/java/com/imdbmovieapp/presentation/screen/homeMoviesFragment/HomeMoviesFragment.kt
+++ app/src/main/java/com/imdbmovieapp/presentation/screen/homeMoviesFragment/HomeMoviesFragment.kt
@@ -3,5 +3,4 @@ package com.imdbmovieapp.presentation.screen.homeMoviesFragment
 import androidx.fragment.app.Fragment
 import com.imdbmovieapp.R
 
-class HomeMoviesFragment : Fragment(R.layout.fragment_home_movies) {
-}
+class HomeMoviesFragment : Fragment(R.layout.fragment_home_movies)
