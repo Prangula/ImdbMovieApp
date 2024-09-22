55e273a8ac866f10fc39330047f5cbf9233e87d8
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 14:56:21 +0400

    remove unnecessary imports

--- app/src/main/java/com/imdbmovieapp/presentation/screen/homeMoviesFragment/HomeMoviesFragment.kt
+++ app/src/main/java/com/imdbmovieapp/presentation/screen/homeMoviesFragment/HomeMoviesFragment.kt
@@ -1,13 +1,7 @@
 package com.imdbmovieapp.presentation.screen.homeMoviesFragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.imdbmovieapp.R
-import com.imdbmovieapp.databinding.FragmentHomeMoviesBinding
-import com.imdbmovieapp.utils.CustomSearchBar
 
 class HomeMoviesFragment : Fragment(R.layout.fragment_home_movies) {
 }
