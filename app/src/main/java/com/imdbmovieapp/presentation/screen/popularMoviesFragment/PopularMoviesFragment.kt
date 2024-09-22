8cb27dab19b1d8ab2fdda24e8ab666c05ea8220f
Luka.Prangulashvili <Luka.prangulashvili@space.ge>
2024-09-22 13:57:34 +0400

    fragment_popular_movies to fragment_home_movies

--- app/src/main/java/com/imdbmovieapp/presentation/screen/popularMoviesFragment/PopularMoviesFragment.kt
+++ app/src/main/java/com/imdbmovieapp/presentation/screen/popularMoviesFragment/PopularMoviesFragment.kt
@@ -3,5 +3,5 @@ package com.imdbmovieapp.presentation.screen.popularMoviesFragment
 import androidx.fragment.app.Fragment
 import com.imdbmovieapp.R
 
-class PopularMoviesFragment : Fragment(R.layout.fragment_popular_movies) {
+class PopularMoviesFragment : Fragment(R.layout.fragment_home_movies) {
 }
